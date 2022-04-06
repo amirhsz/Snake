7d838a1465b7aa5df62d6f7cf84ccd4cc5e60a00
amirhsz <amir1386amir2006@gmail.com>
2022-04-07 01:17:25 +0430

    Make main class to be runnable

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,16 @@
 package inc.faregh;
 
+/**
+ * Main class of our program
+ */
 public class App {
+
+    /**
+     * Main function to run our program
+     * @param args
+     */
+    public static void main(String[] args){
+
+    }
 
 }

ff1505c274fb8db441af2ddc3137a0b9a469d1fd
amirhsz <amir1386amir2006@gmail.com>
2022-04-09 01:46:55 +0430

    Piece class added and implemented completely

---
+++ src/main/java/model/Piece.java
@@ -0,0 +1,40 @@
+package inc.faregh.model;
+//TODO
+/**
+ * it can be Fruit or Body
+ */
+public abstract class Piece {
+
+    /**
+     * location of our object
+     */
+    private Point p;
+
+    /**
+     * make Piece(fruit or body) with a known position
+     * @param p location of object
+     * @see Piece#p
+     */
+    public Piece(Point p) {
+        this.p = p;
+    }
+
+    /**
+     * get position of our object
+     * @return p
+     * @see Piece#p
+     */
+    public Point getP() {
+        return p;
+    }
+
+    /**
+     * set new value for p
+     * @param p new position for our object
+     * @see Piece#p
+     */
+    public void setP(Point p) {
+        this.p = p;
+    }
+
+}

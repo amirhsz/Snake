8e2929db3b5ab0d726b2fb06ce6f0ead71bb755d
amirhsz <amir1386amir2006@gmail.com>
2022-04-09 01:54:52 +0430

    Body done. move method  removed because we can use setP()

---
+++ src/main/java/model/Body.java
@@ -0,0 +1,13 @@
+package inc.faregh.model;
+
+public class Body extends Piece{
+    /**
+     * make body with a known position
+     *
+     * @param p location of object
+     * @see Piece#p
+     */
+    public Body(Point p) {
+        super(p);
+    }
+}
